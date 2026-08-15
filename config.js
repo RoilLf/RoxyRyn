ee9ddb9f1d59bd4c5957ca97d921a7d884e0454c
Zryx <ajithvijiarya2010@gmail.com>
2026-08-15 13:37:15 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,17 @@
+const RoxyRunConfig = {
+    name: "RoxyRun",
+    version: "1.0.0",
+
+    terminal: {
+        prompt: "roxy@run:~$",
+        maxHistory: 100
+    },
+
+    settings: {
+        sound: true,
+        animations: true,
+        autoScroll: true
+    }
+};
+
+export default RoxyRunConfig;
